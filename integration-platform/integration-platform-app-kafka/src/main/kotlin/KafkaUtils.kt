fe150515a715936c31265236646f4cc418a6e534
v.pashnya@oriont.io <v.pashnya@oriont.io>
2026-05-03 15:57:37 +0300

    preparation of a course project, add koin for app-kafka

--- integration-platform/integration-platform-app-kafka/src/main/kotlin/KafkaUtils.kt
+++ integration-platform/integration-platform-app-kafka/src/main/kotlin/KafkaUtils.kt
@@ -1,14 +1,5 @@
 package ru.pvn.integration.platform.kafka
 
-import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.ProducerConfig
-import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.kafka.common.serialization.StringSerializer
-import java.util.Properties
-
-
 data class TopicPair(
   val incoming: String,
   val outgoing: String,
